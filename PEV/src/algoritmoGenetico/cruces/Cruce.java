9d1d2daeed3d15f229ba94d98719a7ec2fd8dd57
Diego <alrp.diego@gmail.com>
2021-03-08 15:32:07 +0100

    Cruce

--- PEV/src/algoritmoGenetico/cruces/Cruce.java
+++ PEV/src/algoritmoGenetico/cruces/Cruce.java
@@ -6,9 +6,11 @@ import algoritmoGenetico.individuos.Individuo;
 
 public abstract class Cruce<E> {
 
-	protected boolean probCruce;
+	protected ArrayList<Integer> selec_cruce;
+	protected double probCruce;
 	protected int punto_cruce;
 	protected int num_selec_cruce;	//Numero de individuos seleccionados para cruzar
+	protected int tamPoblacion;
 	
 	protected abstract ArrayList<Individuo<E>> seleccionadosCruce(ArrayList<Individuo<E>> poblacion);
 	protected abstract ArrayList<Individuo<E>> cruza(ArrayList<Individuo<E>> poblacion);
--- PEV/src/algoritmoGenetico/cruces/CruceMonopunto.java
+++ PEV/src/algoritmoGenetico/cruces/CruceMonopunto.java
@@ -1,11 +1,20 @@
 package algoritmoGenetico.cruces;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import algoritmoGenetico.individuos.Individuo;
+import algoritmoGenetico.individuos.IndividuoFuncion1;
 
 public class CruceMonopunto extends Cruce<Boolean> {
 
+	public CruceMonopunto(double probCruce) {
+		
+		this.num_selec_cruce = 0;
+		this.probCruce = probCruce;
+		this.selec_cruce = new ArrayList<Integer>();
+	}
+	
 	@Override
 	protected ArrayList<Individuo<Boolean>> seleccionadosCruce(ArrayList<Individuo<Boolean>> poblacion) {
 		// TODO Auto-generated method stub
@@ -15,7 +24,45 @@ public class CruceMonopunto extends Cruce<Boolean> {
 	@Override
 	protected ArrayList<Individuo<Boolean>> cruza(ArrayList<Individuo<Boolean>> poblacion) {
 		// TODO Auto-generated method stub
+		
+		this.seleccionIndividuos(poblacion);
+		
+		Random rand = new Random();
+		
+		this.punto_cruce = rand.nextInt(this.tamPoblacion - 2) + 1;
+		
+		for (int i = 0; i < this.num_selec_cruce; i++) {
+			
+		}
 		return null;
 	}
+	
+	protected void cruzaPadres(Individuo<Boolean> padre1, Individuo<Boolean> padre2) {
+		
+		Individuo<Boolean> hijo1 = padre1;
+		Individuo<Boolean> hijo2 = padre2;
+		
+		hijo1.
+	}
 
+	protected void seleccionIndividuos(ArrayList<Individuo<Boolean>> poblacion) {
+		
+		this.tamPoblacion = poblacion.size();
+		Random rand = new Random();
+		
+		for (int i = 0; i < tamPoblacion; i++) {
+			
+			if (this.probCruce > rand.nextInt()) {
+				
+				this.selec_cruce.add(i);
+				this.num_selec_cruce++;
+			}
+		}
+		
+		if ((num_selec_cruce % 2) == 1) {
+			
+			this.num_selec_cruce--;
+			this.selec_cruce.remove(num_selec_cruce);
+		}
+	}
 }
