278e5309e89f51d58cdbfcfecbf8aa7fdd7f981f
Mimel <miguelrobledoblanco@gmail.com>
2021-03-21 21:07:05 +0100

    Boton resetear y bug de grafica

--- PEV/src/interfaz/graficas.java
+++ PEV/src/interfaz/graficas.java
@@ -1,5 +1,7 @@
 package interfaz;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +15,14 @@ public class graficas{
 	
 	graficas(int numGeneraciones){
 		
-		x = new double[numGeneraciones];
-		
-		for(int i=0;i<numGeneraciones;i++) {
-			x[i]=i;
-		}
 		plot = new Plot2DPanel();
 		plot.addLegend("SOUTH");
-		
+		plot.setAxisLabel(0, "Numero de generaciones");
+		plot.setAxisLabel(1, "Fitness");
+		plot.getAxis(0).setLabelFont(new Font("Courier", Font.BOLD, 15));
+		plot.getAxis(1).setLabelFont(new Font("Courier", Font.BOLD, 15));
+		plot.setFont(new Font("Courier", Font.BOLD, 15));
+		plot.getAxis(0).setLabelPosition(0.5, -0.1);
 	}
 	
 	public Plot2DPanel getPlot() {
@@ -30,6 +32,11 @@ public class graficas{
 	public void actualiza(int numGeneraciones, double[] mejorAbsoluto, double[] mejorGeneracion, double[] mediaGeneracion) {
 		
 		plot.removeAllPlots();
+		
+		x = new double[numGeneraciones];
+		for(int i=0;i<numGeneraciones;i++) {
+			x[i]=i;
+		}
 		
 		plot.addLinePlot("Mejor absoluto", x, mejorAbsoluto);
 		plot.addLinePlot("Mejor generacion", x, mejorGeneracion);
