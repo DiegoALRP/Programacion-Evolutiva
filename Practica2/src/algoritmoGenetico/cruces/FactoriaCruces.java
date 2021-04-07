a010c74a0a3d3bb706124abd681342fb99da3693
Diego <alrp.diego@gmail.com>
2021-04-07 15:21:39 +0200

    Creado clases bases

---
+++ Practica2/src/algoritmoGenetico/cruces/FactoriaCruces.java
@@ -0,0 +1,34 @@
+package algoritmoGenetico.cruces;
+
+/**
+ * Universidad Complutense de Madrid.
+ * Programación Evolutiva.
+ * Grupo A 2021.
+ * Profesor:
+ * 	-Carlos Cervigon Ruckauer.
+ * 
+ * Clase Factoria Cruces.
+ * 
+ * @author 
+ * Grupo G06:
+ * 	-Miguel Robledo.
+ * 	-Diego Alejandro Rodríguez Pereira.
+ *
+ */
+
+public class FactoriaCruces {
+
+	/**
+	 * [ES] Patrón Factoria.
+	 * [EN] Factory pattern.
+	 * 
+	 * @param algoritmo	[ES] Tipo de cruce.
+	 * 					[EN] Crossover's type.
+	 * @return 	[ES] La instancia de la clase del tipo de cruce.
+	 * 			[EN] The instance of the class's crossover's type.
+	 */
+	public static Cruce getAlgoritmoDeCruce(String algoritmo) {
+		
+		return null;
+	}
+}
---
+++ Practica2/src/algoritmoGenetico/individuos/FactoriaIndividuo.java
@@ -0,0 +1,38 @@
+package algoritmoGenetico.individuos;
+
+/**
+ * Universidad Complutense de Madrid.
+ * Programación Evolutiva.
+ * Grupo A 2021.
+ * Profesor:
+ * 	-Carlos Cervigon Ruckauer.
+ * 
+ * Clase Factoria Individuo.
+ * 
+ * @author 
+ * Grupo G06:
+ * 	-Miguel Robledo.
+ * 	-Diego Alejandro Rodríguez Pereira.
+ *
+ */
+
+public class FactoriaIndividuo {
+
+	/**
+	 * [ES] Patrón Factoria.
+	 * [EN] Factory pattern.
+	 * 
+	 * @param id	[ES] Función/Tipo del Individuo.
+	 * 				[EN] Individual's Function/Type.
+	 * @param precision	[ES] Valor de precisión.
+	 * 					[EN] Precision's value. 
+	 * @param numN	[ES] Número de genes.
+	 * 				[EN] Gen's number.
+	 * @return	[ES] La instancia de la clase del tipo de individuo.
+	 * 			[EN] The instance of the class's individual's type.
+	 */
+	public static Individuo getIndividuo(String id, double precision, int numN) {
+		
+		return null;
+	}
+}
