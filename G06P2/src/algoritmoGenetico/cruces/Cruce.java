56a4c27f6b54bcafd023242a4f6a07e0145bae25
Diego <alrp.diego@gmail.com>
2021-04-13 00:05:45 +0200

    Comenzado Cruce

--- G06P2/src/algoritmoGenetico/cruces/Cruce.java
+++ G06P2/src/algoritmoGenetico/cruces/Cruce.java
@@ -1,5 +1,10 @@
 package algoritmoGenetico.cruces;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import algoritmoGenetico.individuos.Individuo;
+
 /**
  * Universidad Complutense de Madrid.
  * Programación Evolutiva.
@@ -20,7 +25,43 @@ public abstract class Cruce {
 
 	
 	/**************************** ATRIBUTTES *******************************/
+	protected ArrayList<Integer> selec_cruce;	//Array de enteros que contiene el índice de los individuos de la población
+	protected double probCruce;					//Probabilidad de Cruce
+	protected int punto_cruce;					//Punto de Cruce
+	protected int num_selec_cruce;				//Numero de individuos seleccionados para cruzar
+	protected int tamPoblacion;					//Tamaño de la población
 	/**************************** CONSTRUCTOR *******************************/
 	/***************************** METHODS ********************************/
+	protected void seleccionaIndividuos(ArrayList<Individuo> poblacion) {
+		
+		this.tamPoblacion = poblacion.size();
+		Random rand = new Random();
+		
+		for (int i = 0; i < tamPoblacion; i++) {
+			
+			if (rand.nextDouble() < this.probCruce) {
+
+				if (this.num_selec_cruce % 2 == 1) {
+					
+					if (!poblacion.get(i).getCromosoma().equals(poblacion.get(this.selec_cruce.get(this.num_selec_cruce - 1)).getCromosoma())) {
+						
+						this.selec_cruce.add(i);
+						this.num_selec_cruce++;
+					}
+				}
+				else {
+					
+					this.selec_cruce.add(i);
+					this.num_selec_cruce++;
+				}
+			}
+		}
+		
+		if ((num_selec_cruce % 2) == 1) {
+			
+			this.num_selec_cruce--;
+			this.selec_cruce.remove(num_selec_cruce);
+		}
+	}
 	/**************************** GET & SET ********************************/
 }
---
+++ G06P2/src/algoritmoGenetico/cruces/CrucePorOrden.java
@@ -0,0 +1,96 @@
+package algoritmoGenetico.cruces;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+
+import algoritmoGenetico.individuos.Individuo;
+
+public class CrucePorOrden extends Cruce{
+	
+	private int punto1;
+	private int punto2;
+
+	public void cruza(ArrayList<Individuo> poblacion, double probCruce) {
+		
+		this.num_selec_cruce = 0;
+		this.probCruce = probCruce;
+		this.selec_cruce = new ArrayList<Integer>();
+		this.tamPoblacion = poblacion.size();
+		
+		int longitudCromosoma = 26;
+		
+		this.seleccionaIndividuos(poblacion);
+		
+		Random rand = new Random();
+		punto1 = rand.nextInt(25);
+		punto2 = rand.nextInt(26 - punto1) + punto1;
+		
+		for (int i = 0; i < this.num_selec_cruce; i += 2) {
+			
+			cruzaPadres(poblacion.get(selec_cruce.get(i)), poblacion.get(selec_cruce.get(i + 1)));
+		}
+	}
+	
+	protected void cruzaPadres(Individuo padre1, Individuo padre2) {
+		
+		ArrayList<Integer> cromoPadre1 = padre1.getCromosoma();
+		ArrayList<Integer> cromoPadre2 = padre2.getCromosoma();
+		
+		ArrayList<Integer> cromoPadre1Aux = new ArrayList<Integer>();
+		cromoPadre1Aux.addAll(cromoPadre1);
+		
+		ArrayList<Integer> cromoPadre2Aux = new ArrayList<Integer>();
+		cromoPadre2Aux.addAll(cromoPadre2);
+		
+		ArrayList<Integer> cromoHijo1 = new ArrayList<Integer>();
+		cromoHijo1.addAll(cromoPadre1Aux);
+		HashSet<Integer> setHijo1 = new HashSet<Integer>(26);
+
+		ArrayList<Integer> cromoHijo2 = new ArrayList<Integer>();
+		cromoHijo2.addAll(cromoPadre2Aux);
+		HashSet<Integer> setHijo2 = new HashSet<Integer>(26);
+		
+		int longitudCromo = 26;
+		int longi = this.punto1;
+		int added1, added2 = 0;
+		while (longi < this.punto2) {
+			
+			cromoHijo1.set(longi, cromoPadre2Aux.get(longi));
+			setHijo1.add(cromoPadre2Aux.get(longi));
+			cromoHijo2.set(longi, cromoPadre1Aux.get(longi));
+			setHijo2.add(cromoPadre1Aux.get(longi));
+			longi++;
+			added1++;
+			added2++;
+		}
+		
+		int i1, j1 = (this.punto2 + 1)%26;
+		while (added1 <= 26) {
+			
+			int toAdd = cromoPadre2Aux.get(j1);
+			if (!setHijo1.contains(toAdd)) {
+				
+				cromoHijo1.set(i1, toAdd);
+				i1 = (i1 + 1)%26;
+				added1++;
+			}
+			j1 = (j1 + 1)%26;
+		}
+		
+		int i2, j2 = (this.punto2 + 1)%26;
+		while (added2 <= 26) {
+			
+			int toAdd = cromoPadre2Aux.get(j2);
+			if (!setHijo1.contains(toAdd)) {
+				
+				cromoHijo1.set(i2, toAdd);
+				i2 = (i2 + 1)%26;
+				added2++;
+			}
+			j2 = (j2 + 1)%26;
+		}
+		
+		sustituyePadres(padre1, padre2, cromoHijo1, cromoHijo2, cromoPadre1Aux, cromoPadre2Aux);
+	}
+}
