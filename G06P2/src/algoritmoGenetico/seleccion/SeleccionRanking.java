9cf23dc1ff84499a96b8dbc752cbe286a8ad80da
Diego <alrp.diego@gmail.com>
2021-04-28 09:08:26 +0200

    Comentarios Seleccion

--- G06P2/src/algoritmoGenetico/seleccion/SeleccionRanking.java
+++ G06P2/src/algoritmoGenetico/seleccion/SeleccionRanking.java
@@ -29,12 +29,32 @@ public class SeleccionRanking extends Seleccion{
 	private double[] puntAcumulada;
 	
 	
+	
 	/**************************** CONSTRUCTOR *******************************/
 	public SeleccionRanking() {
 		beta = 2;
 	}
 	
+	
+	
 	/***************************** METHODS ********************************/
+	
+	/**
+	 * [ES]	Esta función es la principal de la clase de Selección.
+	 * Esta función selecciona a los individuos de una población para generar
+	 * una población nueva/auxiliar con los individuos elegidos.
+	 * Ambas poblaciones son del mismo tamaño.
+	 * [EN] This is the main function of the Selection's class.
+	 * This function selects the individuals of a population to generate a new/auxiliar
+	 * population with the individuals chosen.
+	 * Both population are of the same size.
+	 * 
+	 * @param poblacion	[ES] Población de la cual se van a seleccionar los individuos.
+	 * 					[EN] Population form which it's going to be selected the individuals.
+	 * 
+	 * @return	[ES] La nueva población con los individuos seleccionados.
+	 * 			[EN] The new population with the selected individuals.
+	 */
 	@Override
 	public ArrayList<Individuo> seleccionar(ArrayList<Individuo> poblacion) {
 		
