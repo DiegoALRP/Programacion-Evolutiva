56547e9b2e9bd4620e64211a5b8fd4afd421a3ba
Diego <alrp.diego@gmail.com>
2021-05-01 13:08:35 +0200

    Inicio Practica 3

---
+++ G06P3/src/algoritmoGenetico/AlgoritmoGenetico.java
@@ -0,0 +1,33 @@
+package algoritmoGenetico;
+
+
+/**
+ * Universidad Complutense de Madrid.
+ * Programación Evolutiva.
+ * Grupo A 2021.
+ * Profesor:
+ * 	-Carlos Cervigon Ruckauer.
+ * 
+ * Práctica 3
+ * 
+ * Clase Algoritmo Genético.
+ * 
+ * @author 
+ * Grupo G06:
+ * 	-Miguel Robledo.
+ * 	-Diego Alejandro Rodríguez Pereira.
+ *
+ */
+public class AlgoritmoGenetico {
+
+	/******************************** ATRIBUTTES ********************************/
+
+	
+	/******************************* CONSTRUCTOR ********************************/
+	
+	
+	/********************************* METHODS *********************************/
+	
+	
+	/**************************** GETTERS & SETTERS ****************************/
+}
---
+++ G06P3/src/algoritmoGenetico/individuos/Individuo.java
@@ -0,0 +1,33 @@
+package algoritmoGenetico.individuos;
+
+/**
+ * Universidad Complutense de Madrid.
+ * Programación Evolutiva.
+ * Grupo A 2021.
+ * Profesor:
+ * 	-Carlos Cervigon Ruckauer.
+ * 
+ * Práctica 3
+ * 
+ * Clase Individuo.
+ * 
+ * @author 
+ * Grupo G06:
+ * 	-Miguel Robledo.
+ * 	-Diego Alejandro Rodríguez Pereira.
+ *
+ */
+public class Individuo {
+	
+	/******************************** ATRIBUTTES ********************************/
+
+	
+	/******************************* CONSTRUCTOR ********************************/
+	
+	
+	/********************************* METHODS *********************************/
+	
+	
+	/**************************** GETTERS & SETTERS ****************************/
+
+}
