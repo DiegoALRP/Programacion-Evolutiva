0c60757796223f349a410f9de3efbc646912dde9
Mimel <miguelrobledoblanco@gmail.com>
2021-05-05 15:43:04 +0200

    gui

---
+++ prueba/src/prueba/Pair.java
@@ -0,0 +1,21 @@
+package prueba;
+
+public class Pair {
+
+	
+	private int first;
+	private int second;
+	
+	Pair(int f, int s){
+		first = f;
+		second = s;
+	}
+	
+	public int get_first() {
+		return first;
+	}
+	
+	public int get_second() {
+		return second;
+	}
+}
