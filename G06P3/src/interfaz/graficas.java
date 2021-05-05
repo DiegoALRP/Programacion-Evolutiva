8958b61e6675eba4f3d92f68618448e976a7e77f
Mimel <miguelrobledoblanco@gmail.com>
2021-05-05 17:52:15 +0200

    GUI completa y clase Pair

---
+++ G06P3/src/interfaz/graficas.java
@@ -0,0 +1,49 @@
+package interfaz;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import org.math.plot.Plot2DPanel;
+
+public class graficas {
+
+
+	private Plot2DPanel plot;
+	double[] x;
+	
+	public graficas() {
+		
+		plot = new Plot2DPanel();
+		//plot.setLayout(null);
+		//plot.setBounds(400, 10, 1000, 403);
+		plot.addLegend("SOUTH");
+		plot.setAxisLabel(0, "Numero de generaciones");
+		plot.setAxisLabel(1, "Fitness");
+		plot.getAxis(0).setLabelFont(new Font("Courier", Font.BOLD, 15));
+		plot.getAxis(1).setLabelFont(new Font("Courier", Font.BOLD, 15));
+		plot.setFont(new Font("Courier", Font.BOLD, 15));
+		plot.getAxis(0).setLabelPosition(0.5, -0.1);
+	}
+	
+	public Plot2DPanel getPlot() {
+		return this.plot;
+	}
+	
+	public void actualiza(int numGeneraciones, double[] mejorAbsoluto, double[] mejorGeneracion, double[] mediaGeneracion, double[] presionSelectiva) {
+		
+		plot.removeAllPlots();
+		
+		x = new double[numGeneraciones];
+		for(int i=0;i<numGeneraciones;i++) {
+			x[i]=i;
+		}
+		
+		plot.addLinePlot("Mejor absoluto", x, mejorAbsoluto);
+		plot.addLinePlot("Mejor generacion", x, mejorGeneracion);
+		plot.addLinePlot("Media generacion", x, mediaGeneracion);
+		plot.addLinePlot("Presion selectiva", Color.black, x, presionSelectiva);
+
+		
+		
+	}
+}
---
+++ G06P3/src/interfaz/panelHormiga.java
@@ -0,0 +1,41 @@
+package interfaz;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+import Misc.Pair;
+
+public class panelHormiga extends JPanel{
+
+	 public panelHormiga()    {       
+		 setVisible( true );   
+	 } 
+	 
+	public void paint( Graphics g )    {  
+	 for ( int x = 20; x <= 640; x += 20 )
+		 for ( int y = 20; y <= 640; y += 20 ) 
+		 { 
+			 g.setColor(Color.black);
+			 g.drawRect( x, y + 20, 20, 20 );
+			 g.setColor(Color.gray);
+			 g.fill3DRect(x +1, y + 21, 19, 19, true);
+			
+		 }
+	 g.setColor(Color.yellow);
+	 
+	 ArrayList<Pair> a = new ArrayList<Pair>(); // hormiga
+	 Pair p = new Pair(5,5);
+	 Pair p2 = new Pair(8,32);
+	 
+	 a.add(p2);
+	 a.add(p);
+
+	 for (Pair p3 : a ) {
+		 g.fillRect(20 * p3.get_first() + 1, 20 * p3.get_second() + 21, 19, 19);
+
+	 }
+	}
+}
