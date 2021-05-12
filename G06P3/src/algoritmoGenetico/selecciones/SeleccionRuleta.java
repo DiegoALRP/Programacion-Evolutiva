28a1dc41261b28f2eb80c695752388a556a3a79a
Mimel <miguelrobledoblanco@gmail.com>
2021-05-12 15:47:29 +0200

    selecciones

--- G06P3/src/algoritmoGenetico/selecciones/SeleccionRuleta.java
+++ G06P3/src/algoritmoGenetico/selecciones/SeleccionRuleta.java
@@ -1,15 +1,78 @@
 package algoritmoGenetico.selecciones;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import algoritmoGenetico.individuos.Individuo;
 
 public class SeleccionRuleta extends Seleccion{
 
+	/**************************** ATRIBUTTES *******************************/
+	private double[] puntAcumulada;		
+	private double prob;
+	private int pos_super;
+	private double aptitudes;
+	private double acum;
+	
+	
+	/**************************** CONSTRUCTOR *******************************/
+	
+	
+	
+	/***************************** METHODS ********************************/
+	
+	/**
+	 * [ES]	Esta función es la principal de la clase de Selección.
+	 * Esta función selecciona a los individuos de una población para generar
+	 * una población nueva/auxiliar con los individuos elegidos.
+	 * Ambas poblaciones son del mismo tamaño.
+	 * [EN] This is the main function of the Selection's class.
+	 * This function selects the individuals of a population to generate a new/auxiliar
+	 * population with the individuals chosen.
+	 * Both population are of the same size.
+	 * 
+	 * @param poblacion	[ES] Población de la cual se van a seleccionar los individuos.
+	 * 					[EN] Population form which it's going to be selected the individuals.
+	 * 
+	 * @return	[ES] La nueva población con los individuos seleccionados.
+	 * 			[EN] The new population with the selected individuals.
+	 */
 	@Override
 	public ArrayList<Individuo> seleccionar(ArrayList<Individuo> poblacion) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		inicializaAptitudes(poblacion);
+		
+		ArrayList<Individuo> nuevaPoblacion = new ArrayList<Individuo>(poblacion.size());
+		Individuo nuevo;
+		for(int i = 0; i < poblacion.size(); i++) {
+			
+			prob = Math.random();
+			while(pos_super < poblacion.size() && prob > puntAcumulada[pos_super]) {
+				pos_super++;
+			}
+			nuevo = poblacion.get(pos_super);
+			addIndividuo(nuevaPoblacion, nuevo);
+			pos_super = 0;
+		}
+		
+		return nuevaPoblacion;
+	}
+	
+	public void inicializaAptitudes(List<Individuo> poblacion) {
+		
+		aptitudes = 0.0;
+		acum = 0.0;
+		pos_super = 0;
+		puntAcumulada = new double[poblacion.size()];
+		
+		for(Individuo ind : poblacion) {
+			aptitudes += ind.getFitness();
+		}
+		
+		for(int i = 0; i < poblacion.size(); i++) {
+			acum += poblacion.get(i).getFitness() / aptitudes;
+			puntAcumulada[i] = acum;
+		}
 	}
 
 }
--- G06P3/src/algoritmoGenetico/selecciones/SeleccionTorneo.java
+++ G06P3/src/algoritmoGenetico/selecciones/SeleccionTorneo.java
@@ -1,15 +1,82 @@
 package algoritmoGenetico.selecciones;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import algoritmoGenetico.individuos.Individuo;
 
 public class SeleccionTorneo extends Seleccion{
 
+	/**************************** ATRIBUTTES *******************************/
+	private final int tamTorneo = 2;
+	private int tamPoblacion;
+	
+	/**************************** CONSTRUCTOR ******************************/
+	
+	
+	
+	/***************************** METHODS *********************************/
+	
+	/**
+	 * [ES]	Esta función es la principal de la clase de Selección.
+	 * Esta función selecciona a los individuos de una población para generar
+	 * una población nueva/auxiliar con los individuos elegidos.
+	 * Ambas poblaciones son del mismo tamaño.
+	 * [EN] This is the main function of the Selection's class.
+	 * This function selects the individuals of a population to generate a new/auxiliar
+	 * population with the individuals chosen.
+	 * Both population are of the same size.
+	 * 
+	 * @param poblacion	[ES] Población de la cual se van a seleccionar los individuos.
+	 * 					[EN] Population form which it's going to be selected the individuals.
+	 * 
+	 * @return	[ES] La nueva población con los individuos seleccionados.
+	 * 			[EN] The new population with the selected individuals.
+	 */
 	@Override
 	public ArrayList<Individuo> seleccionar(ArrayList<Individuo> poblacion) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		this.tamPoblacion = poblacion.size();
+		ArrayList<Individuo> nuevaPoblacion = new ArrayList<Individuo>();
+		ArrayList<Individuo> candidatos = new ArrayList<Individuo>();
+		
+		Random rand = new Random();
+		int count = 0;
+		while(count < tamPoblacion) {
+			
+			candidatos.clear();
+			
+			for (int i = 0; i < tamTorneo; i++) {
+				
+				candidatos.add(poblacion.get(rand.nextInt(tamPoblacion)));
+			}
+			
+			competenciaTorneo(nuevaPoblacion, candidatos);
+			count++;
+		}
+		
+		return nuevaPoblacion;
 	}
+	
+	public void competenciaTorneo(ArrayList<Individuo> nuevaPoblacion, ArrayList<Individuo> candidatos) {
+		
+		double maxFitness = -Double.MAX_VALUE;
+		int indexBestIndividual = 0;
+		
+		for (int i = 0; i < tamTorneo; i++) {
+			
+			double fitness = candidatos.get(i).getFitness();
+			
+			if (fitness > maxFitness) {
+				
+				maxFitness = fitness;
+				indexBestIndividual = i;
+			}
+		}
+		
+		addIndividuo(nuevaPoblacion, candidatos.get(indexBestIndividual));
+	}
+	
+	/**************************** GETTERS & SETTERS ********************************/
 
 }
