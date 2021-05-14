acbfff7340a22a1fab35b02c0c431ae3fbd4bb9c
Diego <alrp.diego@gmail.com>
2021-05-14 21:21:51 +0200

    Main Prueba Cruce

--- G06P3/src/main/Main.java
+++ G06P3/src/main/Main.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
+import algoritmoGenetico.cruces.Cruce;
+import algoritmoGenetico.individuos.Arbol;
 import algoritmoGenetico.individuos.Individuo;
 import algoritmoGenetico.individuos.Operando;
 import algoritmoGenetico.individuos.RastroSantaFe;
@@ -36,10 +38,74 @@ public class Main {
 			System.out.println(ind.calculateFitness());
 		}*/
 		
-		Operando op1 = new Operando("AVANZA");
+		RastroSantaFe rastro = new RastroSantaFe();
+		Individuo ind1 = new Individuo("Completo", 3, rastro.getComida());
+		Individuo ind2 = new Individuo("Completo", 3, rastro.getComida());
+		
+		ArrayList<Operando> antes1 = new ArrayList<Operando>();
+		ArrayList<Operando> antes2 = new ArrayList<Operando>();
+		ind1.getCromosoma().toArrayAux(antes1);
+		ind2.getCromosoma().toArrayAux(antes2);
+		Cruce cruce = new Cruce();
+		cruce.cruzaPadres(ind1, ind2);
+		
+		ArrayList<Operando> despues1 = new ArrayList<Operando>();
+		ArrayList<Operando> despues2 = new ArrayList<Operando>();
+		ind1.getCromosoma().toArrayAux(despues1);
+		ind2.getCromosoma().toArrayAux(despues2);
+		
+		System.out.println("ANTES1: " + antes1);
+		System.out.println("DESPU1: " + despues1);
+		System.out.println("ANTES2: " + antes2);
+		System.out.println("DESPU2: " + despues2);
+		
+		/*Operando op1 = new Operando("AVANZA");
 		Operando op2 = new Operando("AVANZA");
 		
-		System.out.println(op1.equals(op2));
+		System.out.println(op1.equals(op2));*/
+		
+		/*Operando op1 = new Operando("PROGN2");
+		Arbol a1 = new Arbol(null, op1, 3);
+		
+		ArrayList<Operando> fasti1 = new ArrayList<Operando>();
+		a1.toArrayAux(fasti1);
+		//System.out.println("Arbol antes: " + fasti1);
+		
+		Operando op2 = new Operando("PROGN3");
+		Arbol a2 = new Arbol(null, op2, 3);
+		ArrayList<Operando> fasti2 = new ArrayList<Operando>();
+		a2.toArrayAux(fasti2);
+		//System.out.println("Arbol antes: " + fasti2);
+		
+		ArrayList<Operando> arr1 = new ArrayList<Operando>();
+		
+		Arbol asub1 = a1.getSubTree(0.5);
+		Arbol asub1Padre = asub1.getPadre();
+		int prof1 = asub1.getMax_prof();
+		asub1.toArrayAux(arr1);
+		
+		ArrayList<Operando> arr2 = new ArrayList<Operando>();
+		
+		Arbol asub2 = a2.getSubTree(0.5);
+		Arbol asub2Padre = asub2.getPadre();
+		int prof2 = asub2.getMax_prof();
+		asub2.toArrayAux(arr2);
+		
+		//Arbol copia1 = new Arbol(arr1, prof1);
+		//Arbol copia2 = new Arbol(arr2, prof2);
+		
+		asub1Padre.insertNewTree(asub2, asub1Padre.getIndex(asub1.getRaiz()));
+		asub2Padre.insertNewTree(asub1, asub2Padre.getIndex(asub2.getRaiz()));
+		
+		ArrayList<Operando> pp1 = new ArrayList<Operando>();
+		ArrayList<Operando> pp2 = new ArrayList<Operando>();
+		
+		a1.toArrayAux(pp1);
+		a2.toArrayAux(pp2);
+		System.out.println("Arbol antes: " + fasti1);
+		System.out.println("Arbol despu: " + pp1);
+		System.out.println("Arbol antes: " + fasti2);
+		System.out.println("Arbol despu: " + pp2);*/
 		
 		/*RastroSantaFe rastro = new RastroSantaFe();
 		int numIndividuos = 10;
