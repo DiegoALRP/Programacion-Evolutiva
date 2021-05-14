705df5a7409a5cd3e43f74ef0260ad3dbc98e922
Diego <alrp.diego@gmail.com>
2021-05-14 21:56:22 +0200

    Comienzo Mutacion Funcional Simple

--- G06P3/src/algoritmoGenetico/mutaciones/MutacionFuncionalSimple.java
+++ G06P3/src/algoritmoGenetico/mutaciones/MutacionFuncionalSimple.java
@@ -1,13 +1,1 @@
-package algoritmoGenetico.mutaciones;
 
-import algoritmoGenetico.individuos.Individuo;
-
-public class MutacionFuncionalSimple extends Mutacion{
-
-	@Override
-	protected void mutaIndividuo(Individuo individuo) {
-		// TODO Auto-generated method stub
-		
-	}
-
-}
