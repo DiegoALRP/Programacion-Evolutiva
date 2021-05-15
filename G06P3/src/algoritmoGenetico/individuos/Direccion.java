213c05e7ed047c25fe4c5e54a14b057a58f947f1
Diego <alrp.diego@gmail.com>
2021-05-15 18:37:54 +0200

    Detalles Direccion

--- G06P3/src/algoritmoGenetico/individuos/Direccion.java
+++ G06P3/src/algoritmoGenetico/individuos/Direccion.java
@@ -1,5 +1,22 @@
 package algoritmoGenetico.individuos;
 
+/**
+ * Universidad Complutense de Madrid.
+ * Programación Evolutiva.
+ * Grupo A 2021.
+ * Profesor:
+ * 	-Carlos Cervigon Ruckauer.
+ * 
+ * Práctica 3
+ * 
+ * Clase Enum Direccion.
+ * 
+ * @author 
+ * Grupo G06:
+ * 	-Miguel Robledo.
+ * 	-Diego Alejandro Rodríguez Pereira.
+ *
+ */
 public enum Direccion {
 
 	NORTE, SUR, ESTE, OESTE;
