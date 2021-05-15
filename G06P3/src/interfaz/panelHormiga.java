37c5c4057bd740876e5ed15f4db76a5404bf0b10
Diego <alrp.diego@gmail.com>
2021-05-15 21:33:23 +0200

    Cambiado Panel Hormiga (falta)

--- G06P3/src/interfaz/panelHormiga.java
+++ G06P3/src/interfaz/panelHormiga.java
@@ -7,23 +7,27 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
-import misc.Pair;
+import algoritmoGenetico.misc.Pair;
 
 public class panelHormiga extends JPanel{
 
 	private final static int tamañoCelda = 20;
 	private final static int tamañoTablero = 32;
 	
-	private ArrayList<Pair> casillasComida;
-	private ArrayList<Pair> casillasHormiga;
+	private int[][] tablero;
+	private ArrayList<Pair<Integer, Integer>> camino;
 	
-	 public panelHormiga(ArrayList<Pair> casillasComida)    {       
-		 setVisible(true);   
-		 this.casillasComida = casillasComida;
+	 public panelHormiga(int[][] tablero, ArrayList<Pair<Integer, Integer>> camino) {
+		 
+		 setVisible(true);
+		 this.tablero = tablero;
+		 this.camino = camino;
 	 } 
 	 
 	public void paint(Graphics g)    {  
-		 for ( int x = tamañoCelda; x <= tamañoCelda * tamañoTablero; x += tamañoCelda)
+		
+		 for ( int x = tamañoCelda; x <= tamañoCelda * tamañoTablero; x += tamañoCelda) {
+			 
 			 for ( int y = tamañoCelda; y <= 640; y += tamañoCelda ) 
 			 { 
 				 g.setColor(Color.black);
@@ -32,9 +36,43 @@ public class panelHormiga extends JPanel{
 				 g.fill3DRect(x + 1, y + tamañoCelda + 1, tamañoCelda - 1, tamañoCelda - 1, true);
 				
 			 }
+		 }
 		 g.setColor(Color.black);
 		 
-		 for(Pair p : casillasComida) {
+		 for (int i = 0; i < tamañoTablero; i++) {
+			 for (int j = 0; j < tamañoTablero; j++) {
+				 
+				 if (tablero[i][j] == 1) {
+					 g.fillRect(tamañoCelda * i + tamañoCelda, tamañoCelda * j + tamañoCelda*2, tamañoCelda - 1, tamañoCelda - 1);
+				 }
+			 }
+			 
+		 }
+		 
+		 g.setColor(Color.yellow);
+		 
+		 for (Pair<Integer, Integer> pos : camino) {
+			 
+			 int x = pos.getFirst();
+			 int y = pos.getSecond();
+			 
+			 g.fillRect(tamañoCelda * x + tamañoCelda, tamañoCelda * y + tamañoCelda*2, tamañoCelda - 1, tamañoCelda - 1);
+		 }
+		 
+		 g.setColor(Color.red);
+		 
+		 for (Pair<Integer, Integer> pos : camino) {
+			 
+			 int x = pos.getFirst();
+			 int y = pos.getSecond();
+			 
+			 if (tablero[x][y] == 1) {
+				 
+				 g.fillRect(tamañoCelda * x + tamañoCelda, tamañoCelda * y + tamañoCelda*2, tamañoCelda - 1, tamañoCelda - 1);
+			 }
+		 }
+		 
+		/* for(Pair p : casillasComida) {
 			 g.fillRect(tamañoCelda * p.get_y() + 1, tamañoCelda * p.get_x() + tamañoCelda + 1, tamañoCelda - 1, tamañoCelda - 1);
 		 }
 		 
@@ -52,18 +90,14 @@ public class panelHormiga extends JPanel{
 					 g.fillRect(tamañoCelda * comida.get_y(), tamañoCelda * comida.get_x() + tamañoCelda , tamañoCelda - 1, tamañoCelda - 1);
 				 }
 			 }
-		 }
+		 }*/
 	}
 
-	public void repaint(Graphics g) {
+	/*public void repaint(Graphics g) {
 		g.setColor(Color.yellow);
 		 
 		 for(Pair p : casillasHormiga) {
 			 g.fillRect(tamañoCelda * p.get_y() + 1, tamañoCelda * p.get_x() + tamañoCelda + 1, tamañoCelda - 1, tamañoCelda - 1);
 		 }
-	}
-	
-	public void setCaminoHormiga(ArrayList<Pair> casillas) {
-		this.casillasHormiga = casillas;
-	}
+	}*/
 }
