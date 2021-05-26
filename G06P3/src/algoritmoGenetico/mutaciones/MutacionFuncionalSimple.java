c3ddff59d36a8f8a9baa8ab7df3516b20bb6d748
Diego <alrp.diego@gmail.com>
2021-05-26 20:19:02 +0200

    Comentarios Arbol

--- G06P3/src/algoritmoGenetico/mutaciones/MutacionFuncionalSimple.java
+++ G06P3/src/algoritmoGenetico/mutaciones/MutacionFuncionalSimple.java
@@ -1,6 +1,9 @@
 package algoritmoGenetico.mutaciones;
 
+import java.util.Random;
+
 import algoritmoGenetico.individuos.Individuo;
+import algoritmoGenetico.individuos.Operando;
 
 /**
  * Universidad Complutense de Madrid.
@@ -30,7 +33,61 @@ public class MutacionFuncionalSimple extends Mutacion{
 	@Override
 	public void mutaIndividuo(Individuo individuo) {
 		
-		individuo.mutaFuncionSimple(0.3);
+		individuo.mutaFuncionSimple(this.probMutacion);
+		
+		//Nota:
+		/*
+		 * Al igual que para mutación terminal simple, 
+		 * para este método hemos decidido encargar la mutación a la clase árbol.
+		 * Para que de esta manera esa clase tenga control total de los operadores.
+		 * Y básicamente lo que hace es buscar un nodo función con probabilidad
+		 * igual a la probabilidad de mutación para seleccionar uno de los nodos 
+		 * y lo cambia por otro operador distinto. 
+		 * Hay que tener en cuenta que este cambio se tiene que generar entre operadores
+		 * de la misma anidad
+		 * 
+		 * Dejamos a continuación la parte del código que realiza la clase árbol
+		 * 
+		 */
+		
+		/*public void mutaFuncionSimple(double probMutacion, boolean up) {
+			
+			if (this.raiz.isTerminal()) {
+				this.padre.mutaFuncionSimple(probMutacion, true);
+			}
+			else {
+				if (up) {
+					boolean encontrado = false;
+					for (int i = 0; i < numHijos && !encontrado; i++) {
+						if (this.raiz.equalsProgN2()) {
+							this.raiz = new Operando("SIComida");
+							encontrado = true;
+						}
+						else if (this.raiz.equalsSiComida()) {
+							this.raiz = new Operando("PROGN2");
+							encontrado = true;
+						}
+					}
+					if (!encontrado && this.padre != null) {
+						this.padre.mutaFuncionSimple(probMutacion, true);
+					}
+				}
+				else {
+					Random rand = new Random();
+					if (this.raiz.equalsProgN3() || (rand.nextDouble() > probMutacion)) {
+						this.hijos.get(rand.nextInt(numHijos)).mutaFuncionSimple(probMutacion, up);
+					}
+					else {
+						if (this.raiz.equalsProgN2()) {
+							this.raiz = new Operando("SIComida");
+						}
+						else if (this.raiz.equalsSiComida()) {	
+							this.raiz = new Operando("PROGN2");
+						}
+					}
+				}
+			}
+		}*/
 	}
 
 	/***************************** AUXILIARY METHODS *****************************/
--- G06P3/src/algoritmoGenetico/mutaciones/MutacionTerminalSimple.java
+++ G06P3/src/algoritmoGenetico/mutaciones/MutacionTerminalSimple.java
@@ -1,6 +1,9 @@
 package algoritmoGenetico.mutaciones;
 
+import java.util.Random;
+
 import algoritmoGenetico.individuos.Individuo;
+import algoritmoGenetico.individuos.Operando;
 
 /**
  * Universidad Complutense de Madrid.
@@ -30,6 +33,30 @@ public class MutacionTerminalSimple extends Mutacion{
 	public void mutaIndividuo(Individuo individuo) {
 		
 		individuo.mutaTerminalSimple();
+		
+		//Nota:
+		/*
+		 * Para este método hemos decidido encargar la mutación a la clase árbol.
+		 * Para que de esta manera esa clase tenga control total de los operadores.
+		 * Y básicamente lo que hace es buscar un nodo terminal (es decir que sea hoja)
+		 * y lo cambia por otro operador distinto
+		 * 
+		 * Dejamos a continuación la parte del código que realiza la clase árbol
+		 * 
+		 */
+		
+		/*public void mutaTerminalSimple() {
+			
+			if (this.raiz.isTerminal()) {
+				
+				this.raiz = new Operando(true, this.raiz.toString());
+			}
+			else {
+				
+				Random rand = new Random();
+				hijos.get(rand.nextInt(numHijos)).mutaTerminalSimple();
+			}
+		}*/
 	}
 
 	/***************************** AUXILIARY METHODS *****************************/
